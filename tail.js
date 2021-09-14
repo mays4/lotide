7ed111fa0ec09de9d9ccbc72bcaf88d1763c1e8a
mays4 <maysalani84@yahoo.com>
2021-09-14 12:58:56 -0400

    modified

--- tail.js
+++ tail.js
@@ -27,6 +27,11 @@ const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3);
+const numbers2 = [1,2,3,4,5];
+const result2 = tail(numbers2);
+assertEqual(numbers2.length, 5);
+assertEqual(result2.length, 4);
+assertEqual(result2[0],2);
 
 
 
