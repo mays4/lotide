fa0b468bd118c1febb0ff1e9b2c12a28de9eb24c
mays4 <maysalani84@yahoo.com>
2021-09-20 17:05:50 -0400

    adding test with chai and mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,33 +1,29 @@
-const assertArraysEqual = require(`../assertArraysEqual`);
+const assert = require('chai').assert;
 const middle = require(`../middle`);
 
-//console.log(middle([1]));
-console.log(middle([1, 2]));  // => []
-middle([1, 2]);
+describe("#middle", () => {
+  it("returns 2  for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns 7 and 9  for [7,7,9,6]", () => {
+   
+    assert.deepEqual(middle([7, 7,9, 6]), [7,9]);
+  });
 
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-console.log(middle([1, 2, 3])); 
-console.log(middle([1, 2, 3, 4, 5])); 
-middle([1, 2, 3, 4]); // => [2, 3]
-console.log(middle([1, 2, 3, 4])); 
-middle([1, 2, 3, 4]); // => [3, 4]
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(middle(words));
-
-// no need to capture the return value since we are not checking it
-assertArraysEqual(words.length, 3);
-const numbers2 = [1,2,3,4,5];
-const result2 = middle(numbers2);
-assertArraysEqual(numbers2.length, 5);
-assertArraysEqual(result2.length, 4);
-assertArraysEqual(result2[0],2);
+  
+  it("returns  ['lighthouse'] for ['Yo Yo', 'Lighthouse', 'Labs'] ", () => {
+  
+    assert.deepEqual(middle(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse']);
+  });
+  
+});
 
 
 
-assertArraysEqual(["1", "4", "5"], ["1", "4", "5"]); // Pass
-assertArraysEqual(["1", "7", "3"], ["18", "2", "4"]); // Fail
-assertArraysEqual(["13", "23", "34"], ["178", "2", "3", "4"]); // Fail
-assertArraysEqual(["1", "2", "36"], ["1", "2", 3]); // Fail
+
+
+
+
+
+
+
