7611a6f465e9ba6e6d9fe2fa1354ece509afe305
mays4 <maysalani84@yahoo.com>
2021-09-20 19:42:15 -0400

    modifed code with exports

--- takeUntil.js
+++ takeUntil.js
@@ -11,47 +11,5 @@ const takeUntil = function(array, callback) {
   }
   return results;
 };
+module.exports = takeUntil;
 
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-
-const eqArrays = function(first , second) {
-  
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] !== second[i]) {
-      return false;
-    }
-  }
-  if (first.length !== second.length) {
-    return false;
-  } else if (first === null || second === null) {
-    return false;
-  } else {
-    return true;
-  }
-  
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual ,  expected)) {
-
-    console.log(" ✅✅✅" + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log("🛑🛑🛑" + ` Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-
-
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
