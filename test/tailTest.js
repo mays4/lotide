a3f6a2631230f311fb1ed71d544ab46ac9a6a09d
mays4 <maysalani84@yahoo.com>
2021-09-20 13:21:09 -0400

    add tailTest

---
+++ test/tailTest.js
@@ -0,0 +1,21 @@
+
+const assertEqual = require('../assertEqual');
+// TEST CODE
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+// no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
+const numbers2 = [1,2,3,4,5];
+const result2 = tail(numbers2);
+assertEqual(numbers2.length, 5);
+assertEqual(result2.length, 4);
+assertEqual(result2[0],2);
+
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
+
