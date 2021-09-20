226d2a7cd8844145987009ab1706526f0aee06cb
mays4 <maysalani84@yahoo.com>
2021-09-20 19:21:41 -0400

    adding export

--- letterPositions.js
+++ letterPositions.js
@@ -13,40 +13,8 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
+module.exports = letterPositions;
 
   
 
 
-console.log(letterPositions("hello"));
-console.log(letterPositions("lighthouse in the house"));
-
-
-
-
-const eqArrays = function(first , second) {
-  
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] !== second[i]) {
-      return false;
-    }
-  }
-  if (first.length !== second.length) {
-    return false;
-  } else if (first === null || second === null) {
-    return false;
-  } else {
-    return true;
-  }
-  
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual ,  expected)) {
-
-    console.log(" ✅✅✅" + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log("🛑🛑🛑" + ` Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertArraysEqual(letterPositions("hello").e, [1]);
