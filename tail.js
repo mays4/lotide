89ec2799e9ea6c74a075e60813f92ea771e026e1
mays4 <maysalani84@yahoo.com>
2021-09-20 13:20:45 -0400

    modifying the js  seprateing the assert

--- tail.js
+++ tail.js
@@ -1,11 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(" ✅✅✅" + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log("🛑🛑🛑" + ` Assertion Failed  :${actual} !== ${expected}`);
-  }
-};
-
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(" ✅✅✅" + ` Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log("🛑🛑🛑" + ` Assertion Failed  :${actual} !== ${expected}`);
+//   }
+// };
+const assertEqual = require('./assertEqual');
 const tail = function(num) {
   let arr = [];
   if (num.length <= 1) {
@@ -21,21 +21,5 @@ const tail = function(num) {
   }
 };
 
-// TEST CODE
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-// no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-const numbers2 = [1,2,3,4,5];
-const result2 = tail(numbers2);
-assertEqual(numbers2.length, 5);
-assertEqual(result2.length, 4);
-assertEqual(result2[0],2);
 
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
