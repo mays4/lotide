160a60cb34dd5fc1a03bee1c773b48fc1aebf1f3
mays4 <maysalani84@yahoo.com>
2021-09-20 13:05:53 -0400

    modified the function

--- assertEqual.js
+++ assertEqual.js
@@ -8,9 +8,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
 
+
+
+module.exports = assertEqual;
