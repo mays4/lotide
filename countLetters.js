0e8d930fff6f66fa57d91b02b35ca6331ae72c3d
mays4 <maysalani84@yahoo.com>
2021-09-20 19:22:39 -0400

    adding export

--- countLetters.js
+++ countLetters.js
@@ -1,28 +1,26 @@
 const countLetters = function(sentance) {
   const results = {};
   for (const num of sentance) {
+ 
     num.toLowerCase();
+    if(num !== " "){
+      
+    
+    
     if (results[num]) {
       results[num] += 1;
+
     } else {
       results[num] = 1;
     }
   }
+}
   return results;
 };
+module.exports =  countLetters;
 
 
 
 
 
 
-console.log(countLetters("lighthouse in the house"));
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(" ✅✅✅" + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log("🛑🛑🛑" + ` Assertion Failed  :${actual} !== ${expected}`);
-  }
-};
