1e717c286954074701963eafbbb1fb39c30c95d2
mays4 <maysalani84@yahoo.com>
2021-09-20 19:41:21 -0400

    adding module exports

--- map.js
+++ map.js
@@ -1,12 +1,4 @@
-// const map = function(array, callback) {
-//   const results = [];
-//   for (let item of array) {
-//     console.log('item BEFORE: ', item);
-//     console.log('item AFTER: ', callback(item));
-//     console.log('---');
-//   }
-//   return results;
-//}
+const eqObjects = require("./eqObjects");
 
 const map = function(array, callback) {
   const results = [];
@@ -15,36 +7,4 @@ const map = function(array, callback) {
   }
   return results;
 };
-
-const words = ["ground", "control", "to", "major", "tom"];
-const results1 = map(words, word => word[0]);
-console.log(results1);
-
-const eqArrays = function(first , second) {
-  
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] !== second[i]) {
-      return false;
-    }
-  }
-  if (first.length !== second.length) {
-    return false;
-  } else if (first === null || second === null) {
-    return false;
-  } else {
-    return true;
-  }
-  
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual ,  expected)) {
-
-    console.log(" ✅✅✅" + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log("🛑🛑🛑" + ` Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertArraysEqual(results1, [ 'g', 'c', 'm', 't' ]); 
-assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]); 
+module.exports = map;
