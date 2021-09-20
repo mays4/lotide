67d94ca08ba2a9ac5062c4eb8e96b958a9529372
mays4 <maysalani84@yahoo.com>
2021-09-20 19:40:01 -0400

    modified code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -9,24 +9,6 @@ const findKeyByValue = function(ob,findByValue) {
   }
 };
  
+module.exports = findKeyByValue;
 
 
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(" ✅✅✅" + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log("🛑🛑🛑" + ` Assertion Failed  :${actual} !== ${expected}`);
-  }
-};
-
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre,"Brooklyn Nine-Nine"), "comedy");
